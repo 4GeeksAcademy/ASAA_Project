53fd39d2f01db2fadf8159ce809dc512fb1ac2e8
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2024-03-04 18:39:12 +0000

    first

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -10,6 +10,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { LandingPage } from "./pages/LandingPage";
 
 //create your first component
 const Layout = () => {
@@ -25,7 +26,9 @@ const Layout = () => {
                 <ScrollToTop>
                     <Navbar />
                     <Routes>
-                        <Route element={<Home />} path="/" />
+                       
+                        <Route element={<LandingPage />} path="/" />
+                       
                         <Route element={<Demo />} path="/demo" />
                         <Route element={<Single />} path="/single/:theid" />
                         <Route element={<h1>Not found!</h1>} />
---
+++ src/front/js/pages/LandingPage.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
+import "../../styles/home.css";
+
+
+export const LandingPage = () => {
+  const { store, actions } = useContext(Context);
+
+  return (
+    
+        <div className="container mt-5">
+          <div className="row">
+            <div className="col-md-6 bg-primary text-white">
+              
+              <h2>Columna 1</h2>
+              <p>Contenido de la primera columna...</p>
+            </div>
+            <div className="col-md-6 bg-success text-dark">
+             
+              <h2>Columna 2</h2>
+              <p>Contenido de la segunda columna...</p>
+            </div>
+          </div>
+        </div>
+      );
+    };
