871c469d9fe2a52efcbd288f0eeacf2270d2569e
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2024-03-07 21:51:27 +0000

    fixed media

--- src/front/js/pages/landingpage.js
+++ src/front/js/pages/landingpage.js
@@ -19,12 +19,12 @@ export const LandingPage = () => {
             <div className="d-flex flex-wrap col-12 p-0 m-0 text-white  background-landingpage">
 
                 <div className="columna1 col-6 p-3 ">
-                    <div className="fila1  mb-3 d-flex align-items-center">
+                    <div className="fila1  mb-3 d-flex align-items-center" >
                         <img
                             src="https://play-lh.googleusercontent.com/NciBqxLXBwgKfKDiVCJ9y_FrbEXVtHuIJyLaTNGiIw88S-MJ_iS1442Epi5gD_ghpDg=w240-h480-rw"
                             alt="logo"
-                            className="logo-img" 
-                            width={"150px"}
+                            className="logo-img"
+                            style={{ width: "150px" }}
                         />
                         <div className="ml-3">
                             <span className="text-span">SCAN-ORDER-ENJOY-PAY</span>
@@ -35,35 +35,35 @@ export const LandingPage = () => {
                     </div>
 
                     <div className="fila2">
-                    <ul className="my-3">
-                                    <li>
-                                        <div className="icon">
-                                            <img src={IconImage} alt="Icon" />
-                                        </div>
-                                        <div className="text-column">
-                                            <h4>Sumérgete en una nueva era de comodidad.<br />
-                                                Dile adiós a las esperas.</h4>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="icon">
-                                            <img src={IconImage} alt="Icon" />
-                                        </div>
-                                        <div className="text-column">
-                                            <h4>Rompe con la rutina de esperar.<br />
-                                                Pide y paga en segundos con tecnología QR.</h4>
-                                        </div>
-                                    </li>
-                                    <li>
-                                        <div className="icon">
-                                            <img src={IconImage} alt="Icon" />
-                                        </div>
-                                        <div className="text-column">
-                                            <h4>Tu tiempo es valioso y nosotros lo entendemos.<br />
-                                                Descubre una experiencia sin igual, donde cada segundo cuenta.</h4>
-                                        </div>
-                                    </li>
-                                </ul>
+                        <ul className="my-3">
+                            <li>
+                                <div className="icon">
+                                    <img src={IconImage} alt="Icon" />
+                                </div>
+                                <div className="text-column">
+                                    <h4>Sumérgete en una nueva era de comodidad.<br />
+                                        Dile adiós a las esperas.</h4>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="icon">
+                                    <img src={IconImage} alt="Icon" />
+                                </div>
+                                <div className="text-column">
+                                    <h4>Rompe con la rutina de esperar.<br />
+                                        Pide y paga en segundos con tecnología QR.</h4>
+                                </div>
+                            </li>
+                            <li>
+                                <div className="icon">
+                                    <img src={IconImage} alt="Icon" />
+                                </div>
+                                <div className="text-column">
+                                    <h4>Tu tiempo es valioso y nosotros lo entendemos.<br />
+                                        Descubre una experiencia sin igual, donde cada segundo cuenta.</h4>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
 
                     <div className="fila3">
@@ -80,20 +80,40 @@ export const LandingPage = () => {
                     </div>
                 </div>
 
-                <div className="columna2 col-6 p-3 ">
-                    <div className="container">
-                        <img src={QRBackground} alt="Icon" className="qr-img" />
+                <div className="columna2 col-6 p-3  ">
+                    <img src={QRBackground} alt="Icon" className="qr-background"  />
+                    <div className="scene">
+                        <div className="phone__wrapper">
+                            <div className="phone__frame">
+                                <div className="camera">
+                                    <div className="camera__lens"></div>
+                                </div>
+                                <div className="screen">
+                                    <div className="time">
+                                        <span>15:11</span>
+                                    </div>
+                                    <div className="battery">
+                                        <div className="battery__life"></div>
+                                    </div>
+                                    <div className="screen__active"></div>
+                                    <div className="screen__time">
+                                        <span>¡Hola!</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
+                
+                
+
+
                 </div>
             </div>
-
             <ComoFunciona />
             <Ventajas />
             <Afiliados />
             <ContactForm />
+
         </>
     );
 };
-
-
-
