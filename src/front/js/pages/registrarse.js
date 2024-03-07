de512bfd3fc12e5e873ef8fd3b826a5d220f501d
Alex Fazakas <144437816+alexcf04@users.noreply.github.com>
2024-03-07 19:05:20 +0000

    prueba login

--- src/front/js/pages/registrarse.js
+++ src/front/js/pages/registrarse.js
@@ -1,33 +1,156 @@
-import React from "react";
+import React, { useState, useContext } from "react";
+import { Context } from "../store/appContext";
+import { Link, useNavigate } from "react-router-dom";
+import "../../styles/form.css";
 
-export const Register = () => (
-  <div className="container border text-white">
-    <h1>Crea tu cuenta</h1>
-    <span>Escribe tu nombre, email y contraseña para registrarte</span>
-    <form action="/registro" method="post">
-      <label  htmlFor="nombre">Nombre:</label><br/>
-      <input type="text" id="nombre" name="nombre" required /><br/><br/>
+export const SignUp = () => {
+  const { actions } = useContext(Context);
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const [pwShown, setPwShown] = useState(false);
 
-      <label htmlFor="email">Email:</label><br/>
-      <input type="email" id="email" name="email" required /><br/><br/>
+  const navigate = useNavigate();
 
-      <label htmlFor="password">Contraseña:</label><br/>
-      <input type="password" id="password" name="password" required /><br/><br/>
+  const handleSignup = async () => {
+    try {
+      const success = await actions.newUser(user);
+      if (success) {
+        navigate("/login"); // Redirecciona a la página de inicio de sesión si el registro tiene éxito
+      } else {
+        alert("Sign up failed. Please try again."); // Si falla el registro, mostrar mensaje de error
+        setUser({
+          ...user,
+          password: "", // Limpiar el campo del password
+        });
+      }
+    } catch (error) {
+      alert("Error during Sign Up:", error);
+    }
+  };
 
-      <input className="col-12" type="submit" value="Registrar" />
-    </form>
-    <div className="container">
-        <span>
-            Al registrarse estas de acuerdo con nuestras condiciones y politica de privacidad 
-        </span>
-            <button className="col-12 my-2"> 
-                connect with facebook
-            </button>
-            <button className="col-12 my-2"> 
-                connect with google
-            </button>
+  const showHidePassword = () => {
+    setPwShown(!pwShown);
+  };
+
+  return (
+    <div
+      className="text-center"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "50px",
+        marginBottom: "20px",
+      }}
+    >
+      <div className="animated-container">
+        <div className="wave"></div>
+        <div className="wave"></div>
+        <div className="wave"></div>
+
+        <form>
+          <div className="container">
+            <header className="head-form mt-5 text-white">
+              <h2>Inicia sesion</h2>
+            </header>
+
+            <div className="field-set mt-5">
+              <div className="input-group mb-3">
+                <span
+                  className="input-group-text"
+                  style={{ background: "white", borderRight: "none" }}
+                >
+                  <i className="fa fa-user-circle"></i>
+                </span>
+
+                <input
+                  style={{
+                    background: "white",
+                    borderLeft: "none",
+                    borderRight: "none",
+                  }}
+                  type="email"
+                  className="form-control mx-auto"
+                  value={user.email}
+                  placeholder="@ Email"
+                  onChange={(event) => {
+                    setUser({
+                      ...user,
+                      email: event.target.value,
+                    });
+                  }}
+                  required
+                />
+              </div>
+
+              <div className="input-group mb-3">
+                <span
+                  className="input-group-text"
+                  style={{ background: "white", borderRight: "none" }}
+                >
+                  <i className="fa fa-key"></i>
+                </span>
+                <input
+                  type={pwShown ? "text" : "password"}
+                  style={{
+                    background: "white",
+                    borderLeft: "none",
+                    borderRight: "none",
+                  }}
+                  className="form-control mx-auto"
+                  placeholder="Password"
+                  value={user.password}
+                  onChange={(event) => {
+                    setUser({
+                      ...user,
+                      password: event.target.value,
+                    });
+                  }}
+                />
+                <span
+                  className="input-group-text eye-icon"
+                  onClick={showHidePassword}
+                  style={{ background: "white", borderLeft: "none" }}
+                >
+                  <i
+                    className={`fa ${pwShown ? "fa-eye-slash" : "fa-eye"}`}
+                    aria-hidden="true"
+                    type="button"
+                    id="eye"
+                    style={{ backgroundColor: "transparent" }}
+                  ></i>
+                </span>
+              </div>
+
+              <div className="text-center pt-1 mb-3 pb-1 d-flex flex-column">
+                <button
+                  className="btn mb-1 btn-primary"
+                  type="button"
+                  onClick={handleSignup}
+                >
+                  Inicia sesion
+                </button>
+              </div>
+
+              <span className="text-white">
+                Already have an account?{" "}
+                <Link
+                  to="/login"
+                  style={{
+                    textDecoration: "none",
+                    fontWeight: "bold",
+                    color: "blue",
+                  }}
+                >
+                  Date de alta
+                </Link>
+              </span>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
-
-export default Register;
+  );
+};
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,56 +1,130 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+	  store: {
+		message: null,
+		demo: [
+		  {
+			title: "FIRST",
+			background: "white",
+			initial: "white",
+		  },
+		  {
+			title: "SECOND",
+			background: "white",
+			initial: "white",
+		  },
+		],
+	  },
+	  actions: {
+		// Use getActions to call a function within a fuction
+		exampleFunction: () => {
+		  getActions().changeColor(0, "green");
 		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
+  
+		getMessage: async () => {
+		  try {
+			// fetching data from the backend
+			const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+			const data = await resp.json();
+			setStore({ message: data.message });
+			// don't forget to return something, that is how the async resolves
+			return data;
+		  } catch (error) {
+			console.log("Error loading message from backend", error);
+		  }
+		},
+		changeColor: (index, color) => {
+		  //get the store
+		  const store = getStore();
+  
+		  //we have to loop the entire demo array to look for the respective index
+		  //and change its color
+		  const demo = store.demo.map((elm, i) => {
+			if (i === index) elm.background = color;
+			return elm;
+		  });
+  
+		  //reset the global store
+		  setStore({ demo: demo });
+		},
+  
+		syncTokenFromSessionStore: () => {
+		  const token = sessionStorage.getItem("token");
+		  const user = sessionStorage.getItem("user");
+		  const idUser = sessionStorage.getItem("idUser");
+		  if (token && token != "" && token != undefined) {
+			setStore({ token: token, user: user, idUser: idUser });
+		  }
+		},
+  
+		login: async (credentials) => {
+		  try {
+			console.log(credentials);
+			const resp = await fetch(process.env.BACKEND_URL + "/api/login", {
+			  method: "POST",
+			  headers: {
+				"Content-Type": "application/json",
+			  },
+			  body: JSON.stringify(credentials),
+			});
+			const data = await resp.json();
+			console.log(resp.status);
+			if (resp.status != 200) return false;
+			console.log(data);
+			sessionStorage.setItem("token", data.access_token);
+			sessionStorage.setItem("user", data.user);
+			sessionStorage.setItem("idUser", data.idUser);
+			setStore({
+			  token: data.access_token,
+			  user: data.user,
+			  idUser: data.idUser,
+			});
+  
+			return true;
+		  } catch (error) {
+			console.log("Error loading message from backend", error);
+			setStore({ token: null, user: null, idUser: null });
+			sessionStorage.removeItem("token");
+			sessionStorage.removeItem("user");
+			sessionStorage.removeItem("idUser");
+		  }
+		},
+  
+		logout: () => {
+		  sessionStorage.removeItem("token");
+		  sessionStorage.removeItem("user");
+		  setStore({
+			token: null,
+			user: null,
+		  });
+		},
+  
+		newUser: async (user) => {
+		  let data = "";
+		  console.log(user);
+		  const respUser = await fetch(process.env.BACKEND_URL + "/api/signup", {
+			method: "POST",
+			headers: {
+			  "Content-Type": "application/json",
 			},
-
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color   
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;   
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			},
-
-			
-		}
+			body: JSON.stringify(user),
+		  });
+		  /*data = await respUser.json();
+				  console.log(respUser);*/
+		  if (respUser.status != 200) return false;
+		  return true;
+		},
+  
+		gotopage: (toPage) => {
+		  const store = getStore();
+		  if (!store.token) {
+			return "/login";
+		  } else {
+			return toPage;
+		  }
+		},
+	  },
 	};
-};
-
-export default getState;
+  };
+  
+  export default getState;
