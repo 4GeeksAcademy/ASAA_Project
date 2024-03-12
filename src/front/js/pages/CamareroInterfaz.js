2664499cb1f8920c54e6616d65bfa3d53ae8c5c3
Sara Pages <144797101+SPRDesign@users.noreply.github.com>
2024-03-12 14:48:37 +0000

    edit profile

--- src/front/js/pages/CamareroInterfaz.js
+++ src/front/js/pages/CamareroInterfaz.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import "../../styles/camarero_interfaz.css";
 
 
-
 export const CamareroInterfaz = () => {
 
     const [showDetailsParis, setShowDetailsParis] = useState(false);
